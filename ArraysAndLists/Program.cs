aaccf022bbdfced6f0746634d24c98e3b30c35fe
ReggieMW <reggiewoods@msn.com>
2025-03-11 11:26:07 -0500

    coompleted exercise to list evens and odds.

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -10,15 +11,19 @@ namespace ArraysAndLists
 
             // Create an int array and populate it with numbers 1-10
             
+            int[] startingTen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create a list of type int
              * Name the list "evens"
              */
 
+            List<int> evens = new List<int>();
             
             /* Create another list of type int
              * Name the list "odds"
              */
+            
+            List<int> odds = new List<int>();
             
 
              /* Using either a foreach or for loop,
@@ -29,16 +34,39 @@ namespace ArraysAndLists
              * If the number is odd, add it to the odds list.
              */
 
+             foreach(int i in startingTen)
+             {
+                 if (i % 2 == 0)
+                 {
+                     evens.Add(i);
+                 }
+                 else
+                 {
+                     odds.Add(i);
+                 }
+             }
+             
+
 
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
+
+            foreach (int i in evens)
+            {
+                Console.WriteLine(i);
+            }
             
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.WriteLine(odds[i]);
+            }
         }
     }
 }
